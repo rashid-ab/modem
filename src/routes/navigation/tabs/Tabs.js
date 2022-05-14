9d933768f897b05788dff249aba49135e25565e1
rashid-ab <rashidbuttuog@gmail.com>
2022-05-14 05:03:35 +0500

    init

--- src/routes/navigation/tabs/Tabs.js
+++ src/routes/navigation/tabs/Tabs.js
@@ -160,7 +160,7 @@ const TabNavigator = () => {
         showLabel: false,
         style: {
           backgroundColor: '#000000',
-          height: Platform.OS === 'ios' ? 85 : 75,
+          height: Platform.OS === 'ios' ? 90 : 80,
           paddingTop: Platform.OS === 'ios' ? 0 : 10,
           paddingBottom: 10,
         },
