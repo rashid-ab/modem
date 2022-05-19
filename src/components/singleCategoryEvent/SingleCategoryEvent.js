6b67addf4cc0a78878f3287191906fda19d72368
rashid-ab <rashidbuttuog@gmail.com>
2022-05-20 02:27:45 +0500

    init

--- src/components/singleCategoryEvent/SingleCategoryEvent.js
+++ src/components/singleCategoryEvent/SingleCategoryEvent.js
@@ -142,7 +142,7 @@ const styles = StyleSheet.create({
   row: {
     flexDirection: 'row',
     alignItems: 'center',
-    paddingHorizontal: 10
+    // paddingHorizontal: 10
   },
   contactName: {
     color: 'black',
